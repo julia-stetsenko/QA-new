2d858ba8b8b1103410420fe89abc5396765f51da
Julia Stetsenko <julia.hizh@gmail.com>
2022-03-16 17:44:36 +0200

    is closer to 10

--- src/HW_Java_1/is_closer_to_10.java
+++ src/HW_Java_1/is_closer_to_10.java
@@ -3,7 +3,7 @@ package HW_Java_1;
 public class is_closer_to_10 {
     public static void main(String[] args) {
     double m = 8.5;
-    double n = 11.2;
+    double n = 11.1;
         double m_one = Math.abs(m - 10);
         double n_one = Math.abs(n - 10);
         if (m_one > n_one){
